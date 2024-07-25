eb8631275854f833a255730adb4c3d2a6d3ce7b1
Rutvik Gondekar <gondekarrutvik@gmail.com>
2024-07-25 14:15:24 +0530

    completed sec-4

--- sec-4/app.js
+++ sec-4/app.js
@@ -1,19 +1,31 @@
 const express = require('express');
+const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  // res.status(200).send('Hello from the server side!');
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
+// 1. Middleware
+// console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware.');
+  next();
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}....`);
-});
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
